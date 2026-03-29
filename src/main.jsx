670f7a6bff601d868e86c1c544446a84b69684c5
GreatWayne-ILEARN <madwisdom25@gmail.com>
2026-03-29 11:56:28 +0100

    update import statement in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext.jsx";
+import { CartProvider } from "./Context/CartContext";
 
 
 createRoot(document.getElementById("root")).render(
